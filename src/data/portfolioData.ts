0207596bfa86b7f3610f6d513e3faf5c8a4cb9c4
Devansh Soni <devanshsoni1094@gmail.com>
2026-09-22 12:15:12 +0530

    update resume URL to new Google Drive link

--- src/data/portfolioData.ts
+++ src/data/portfolioData.ts
@@ -81,7 +81,7 @@ export const portfolioData = {
       minor: "Computer Science",
       ambition: "Long-term focus on Data Science and Machine Learning."
     },
-    resumePdf: "https://drive.google.com/file/d/11-XEVQAozm7UxUHZtmPK2zBV6e6bNw6C/view?usp=drive_link",
+    resumePdf: "https://drive.google.com/file/d/1U7MEOAKHEE1ygTKjr36Vn-mhpdhGjIVp/view?usp=drive_link",
     contact: {
       phone: "+91-7737378928",
       email: "devanshsoni1094@gmail.com",
